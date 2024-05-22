a54c44b3c79922af7247145da9234deda27ea135
Otávio <otaviooasc@gmail.com>
2024-05-21 21:44:03 -0300

    Api de processos

--- src/main/java/com/desafio/fsbr/controller/ProcessosController.java
+++ src/main/java/com/desafio/fsbr/controller/ProcessosController.java
@@ -1,15 +1,21 @@
 package com.desafio.fsbr.controller;
 
-import com.desafio.fsbr.dto.ProcessoDTO;
+import com.desafio.fsbr.exception.ResourceNotFoundException;
 import com.desafio.fsbr.model.ProcessoModel;
 import com.desafio.fsbr.service.ProcessoService;
-import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 
 @RestController
@@ -18,20 +24,58 @@ public class ProcessosController {
     @Autowired
     private ProcessoService service;
 
+    @Value("${api.pasta.upload}")
+    private String uploadDir;
+
     @PostMapping("/salvar")
-    public ResponseEntity<Object> salvarProcesso(@RequestBody @Valid ProcessoDTO dto) {
+    public ResponseEntity<Object> salvarProcesso(
+            @RequestParam @NotBlank String npu,
+            @RequestParam @NotBlank String municipio,
+            @RequestParam @NotBlank String uf,
+            @RequestParam("documentoPdf") @NotNull MultipartFile documentoPdf) throws IOException {
         try {
-            var processoModel = new ProcessoModel();
-            BeanUtils.copyProperties(dto, processoModel);
-            processoModel.setDataVisualizacao(LocalDate.now());
-            return ResponseEntity.status(HttpStatus.CREATED).body(service.salvarProcesso(processoModel));
+            String fileName = salvarUploadPDF(documentoPdf);
+
+            ProcessoModel processo = new ProcessoModel();
+            processo.setNpu(npu);
+            processo.setDataVisualizacao(LocalDate.now());
+            processo.setMunicipio(municipio);
+            processo.setUf(uf);
+            processo.setDocPDF(fileName);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.salvarProcesso(processo));
         }catch (Exception ex) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao salvar: " + ex.getMessage());
         }
     }
 
+    private String salvarUploadPDF(MultipartFile documentoPdf) throws IOException {
+        if (!documentoPdf.isEmpty()) {
+            byte[] bytes = documentoPdf.getBytes();
+            Path path = Paths.get(uploadDir + documentoPdf.getOriginalFilename());
+            Files.write(path, bytes);
+            return path.toString();
+        }
+        return null;
+    }
+
     @GetMapping("/listar")
     public ResponseEntity<Object> listarProcessos(@RequestParam int page, @RequestParam int size) {
         return ResponseEntity.ok().body(service.listarProcessos(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> obterProcesso(@PathVariable String id) {
+        try {
+            return ResponseEntity.ok().body(service.obterProcesso(id));
+        }catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProcesso(@PathVariable String id) {
+        service.deletarProcesso(id);
+        return ResponseEntity.noContent().build();
     }
 }
